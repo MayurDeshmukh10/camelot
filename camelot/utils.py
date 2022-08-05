421113d563850c184165b8ad4e34dbe629eb8d24
Mayur Deshmukh <mayur.deshmukh.vc@startv.com>
2022-08-05 18:07:57 +0530

    Handle index error

--- camelot/utils.py
+++ camelot/utils.py
@@ -871,7 +871,9 @@ def split_textline(table, textline, direction, flag_size=False, strip_text=""):
                             break
                         # TODO: add test
                         if cut == x_cuts[-1]:
-                            cut_text.append((r, cut[0] + 1, obj))
+                            # cut_text.append((r, cut[0] + 1, obj))
+                            new_idx = min(cut[0] + 1, len(table.cols) - 1)
+                            cut_text.append((r, new_idx, obj))
                     elif isinstance(obj, LTAnno):
                         cut_text.append((r, cut[0], obj))
         elif direction == "vertical" and not textline.is_empty():
@@ -903,7 +905,9 @@ def split_textline(table, textline, direction, flag_size=False, strip_text=""):
                             break
                         # TODO: add test
                         if cut == y_cuts[-1]:
-                            cut_text.append((cut[0] - 1, c, obj))
+                            # cut_text.append((cut[0] - 1, c, obj))
+                            new_idx = max(cut[0] - 1, 0)
+                            cut_text.append((new_idx, c, obj))
                     elif isinstance(obj, LTAnno):
                         cut_text.append((cut[0], c, obj))
     except IndexError:
